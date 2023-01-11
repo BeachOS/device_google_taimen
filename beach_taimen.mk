407504e3026e081e89a73165ffb54f3cef9a1829
root <Beach OS>
2023-01-11 08:09:31 +0000

    BeachOS

---
+++ beach_taimen.mk
@@ -0,0 +1,21 @@
+# Inherit some common stuff.
+$(call inherit-product, vendor/beach/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/wahoo/device-beach.mk)
+$(call inherit-product, device/google/taimen/aosp_taimen.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := beach_taimen
+PRODUCT_MODEL := Pixel 2 XL
+PRODUCT_BRAND := google
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=taimen \
+    PRIVATE_BUILD_DESC="taimen-user 11 RP1A.201005.004.A1 6934943 release-keys"
+
+BUILD_FINGERPRINT := google/taimen/taimen:11/RP1A.201005.004.A1/6934943:user/release-keys
+
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_SOONG_NAMESPACES += vendor/google_devices/taimen
